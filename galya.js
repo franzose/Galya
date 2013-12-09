b178095f45a48bf28e33406bbc0f5b8fb861ff37
Jan Iwanow <iwanow.jan@gmail.com>
2013-12-10 02:13:19 +1100

    wrote basic features

---
+++ galya.js
@@ -0,0 +1,360 @@
+(function($, undefined){
+    $.fn.galya = function(args){
+        //
+        var settings = {
+            //animation: 'fade', not implemented
+            autoplay: false,
+
+            // fadeIn, fadeOut speed
+            fadeSpeed: 500,
+
+            // thumbnails container left-right scroll
+            scrollSpeed: 250,
+
+            slideWidth: 725,
+            slideHeight: 'auto', //not implemented
+
+            // whether to crop when a slide is larger
+            // than its container
+            slideCrop: false, //not implemented
+
+            // sides to crop from
+            slideCropBy: ['bottom', 'right'], //not implemented
+
+            // caption heading level: 1-6
+            headingLevel: 4,
+
+            // caption content wrapping tag: div, p, whatever
+            captionContentTag: 'p',
+
+            thumbnailWidth: 110,
+            thumbnailHeight: 'auto' //not implemented
+        };
+
+        args && $.extend(settings, args);
+
+        this.each(function(){
+            new Galya($(this), settings).initialize();
+        });
+
+        return this;
+    }
+
+    /**
+     *
+     *
+     * @constructor
+     * @param $container
+     * @param settings
+     */
+    function Galya($container, settings){
+        //
+        var classes = {
+            // DIV that wraps all slides
+            slidesContainer: 'galya-slides',
+
+            // the slide itself
+            slide: 'galya-slide',
+
+            // image inside the slide
+            slideImage: 'galya-image',
+
+            // wrapper for the slide description
+            slideCaption: 'galya-caption',
+
+            // heading of the slide description
+            slideHeading: 'galya-caption-heading',
+
+            // content of the slide description
+            slideText: 'galya-caption-content',
+
+            // DIV that wraps all thumbnails
+            thumbsContainer: 'galya-thumbnails',
+
+            // scrolling DIV
+            thumbsScrollable: 'galya-thumbnails-scrollable',
+
+            // the thumbnail image
+            thumbImage: 'galya-thumbnail',
+
+            // 'previous slide' control
+            prev: 'galya-prev',
+
+            active: 'active'
+        };
+
+        //
+        var $objects = {};
+
+        //
+        var props = {};
+
+        /**
+         *
+         */
+        this.initialize = function(){
+            $objects.container = $container;
+            $objects.sourceImages = $('> img', $container);
+
+            renderSlidesList();
+            renderThumbnailsList();
+            renderNavControls();
+
+            $objects.slidesContainer = $('.'+classes.slidesContainer, $container);
+            $objects.slides = $('.'+classes.slide, $container);
+            $objects.images = $('.'+classes.slideImage, $container);
+            $objects.thumbsContainer = $('.'+classes.thumbsContainer, $container);
+            $objects.thumbsScrollable = $('.'+classes.thumbsScrollable, $container);
+            $objects.thumbs = $('.'+classes.thumbImage, $container);
+            $objects.prev = $('.'+classes.prev, $container);
+
+            adjustSizes();
+            activateStage(0);
+
+            $objects.images.first().load(function(){
+                var imageHeight = $(this).height();
+
+                $objects.slidesContainer.height(imageHeight);
+                $objects.thumbsContainer.height($objects.thumbs.parent().height());
+                $container.height(imageHeight+$objects.thumbsContainer.height());
+                $objects.prev.height(imageHeight);
+            });
+
+            //
+            $objects.images.on('click', function(ev){
+                if ($objects.thumbsScrollable.is(':animated'))
+                    return false;
+
+                activateStage('next');
+            });
+
+            //
+            $objects.thumbs.on('click', function(){
+                var $this = $(this);
+
+                if ($this.hasClass(classes.active))
+                    return false;
+
+                activateStage($this.index());
+            });
+
+            //
+            $objects.prev.on('click', function(){
+                if ($objects.thumbsScrollable.is(':animated'))
+                    return false;
+
+                activateStage('prev');
+            });
+        };
+
+        function adjustSizes(){
+            $objects.container.width(settings.slideWidth);
+            $objects.thumbs.width(settings.thumbnailWidth);
+
+            var containerWidth = $objects.container.width();
+            var thumbWidth = $objects.thumbs.width();
+            props.visibleThumbs = Math.floor(containerWidth / thumbWidth);
+            var thumbsMarginLeft = parseInt($objects.thumbs.last().css('margin-left')) * (props.visibleThumbs-1);
+            props.initialOffset = (thumbWidth * (props.visibleThumbs-1) + thumbsMarginLeft);
+        }
+
+        //
+        function getImageAttrs($image){
+            return {
+                src:     $image.attr('src'),
+                alt:     $image.attr('alt') || '',
+                title:   $image.data('title') || '',
+                content: $image.data('content') || ''
+            }
+        }
+
+        // Render slides
+        function renderSlidesList(){
+            var html = '';
+
+            $objects.sourceImages.each(function(idx, image){
+                var $image = $(image);
+                var attrs = getImageAttrs($image);
+
+                var figureHTML = '<figure class="'+classes.slide+'">'+
+                    '<img class="'+classes.slideImage+'" src="'+attrs.src+'" alt="'+attrs.alt+'">';
+
+                if (attrs.alt || attrs.content){
+                    figureHTML += '<figcaption class="'+classes.slideCaption+'">';
+
+                    if (attrs.title){
+                        figureHTML += '<h'+settings.headingLevel+' class="'+classes.slideHeading+'">'+
+                            attrs.title+'</h'+settings.headingLevel+'>';
+                    }
+
+                    var content = (attrs.content ? attrs.content : attrs.alt);
+
+                    content = '<'+settings.captionContentTag+' class="'+classes.slideText+'">'+
+                        content+'</'+settings.captionContentTag+'>';
+
+                    figureHTML += content+'</figcaption>';
+                }
+
+                html += figureHTML+'</figure>';
+            });
+
+            $objects.container.html('<div class="'+classes.slidesContainer+'">'+html+'</div>');
+        }
+
+        // Renders thumbnails for the slides
+        function renderThumbnailsList(){
+            var html = '';
+
+            $objects.sourceImages.each(function(index, image){
+                var $image = $(image);
+                var attrs = getImageAttrs($image);
+
+                html += '<img class="'+classes.thumbImage+'" src="'+attrs.src+'" alt="'+attrs.alt+'">';
+            });
+
+            $objects.container.append('<div class="'+classes.thumbsContainer+'">'+
+                '<div class="'+classes.thumbsScrollable+'">'+html+'</div></div>');
+        }
+
+        // Renders navigation controls
+        function renderNavControls(){
+            $objects.container.append('<div class="'+classes.prev+'"><span>&#x276e;</span></div>');
+        }
+
+        //
+        function getStageByIndex(index){
+            return {
+                slide: getElementByIndex($objects.slides, index),
+                thumb: getElementByIndex($objects.thumbs, index)
+            }
+        }
+
+        // Activates a stage (slide and its thumbnail)
+        function activateStage(direction){
+            var $stage = getStageByIndex(getStageIndexByDirection(direction));
+            props.stageIndex = $stage.slide.index();
+            props.oldStageIndex = deactivateStage();
+
+            activateSlide($stage.slide);
+            activateThumb($stage.thumb);
+            scrollThumbs(direction);
+        }
+
+        //
+        function activateSlide($slide){
+            $slide.fadeIn(settings.fadeSpeed, function(){
+                $(this).addClass(classes.active);
+            });
+        }
+
+        //
+        function activateThumb($thumb){
+            $thumb.addClass(classes.active);
+        }
+
+        //
+        function deactivateStage(){
+            deactivateSlide();
+            return deactivateThumb();
+        }
+
+        //
+        function deactivateSlide(){
+            var $element = getActiveElement($objects.slides);
+            var index = $element.index();
+
+            $element.fadeOut(settings.fadeSpeed, function(){
+                $(this).removeClass(classes.active);
+            });
+
+            return index;
+        }
+
+        //
+        function deactivateThumb(){
+            var $element = getActiveElement($objects.thumbs);
+            var index = $element.index();
+
+            $element.removeClass(classes.active);
+
+            return index;
+        }
+
+        // Gets a slide that is currently being shown
+        function getActiveElement($collection){
+            return $collection.filter(function(idx, el){
+                return $(el).hasClass(classes.active);
+            }).first();
+        }
+
+        //
+        function getElementByIndex($collection, index){
+            return $collection.filter(function(idx, el){
+                return $(el).index() == index;
+            }).first();
+        }
+
+        //
+        function getStageIndexByDirection(direction){
+            var activeIndex = getActiveElement($objects.slides).index();
+
+            switch(direction){
+                case 'prev':
+                    --activeIndex;
+                    break;
+
+                case 'next':
+                    ++activeIndex;
+                    break;
+
+                default:
+                    activeIndex = direction; // if the value is integer
+                    break;
+            }
+
+            var min = 0;
+            var max = $objects.slides.length-1;
+
+            if (activeIndex > max){
+                activeIndex = min;
+            }
+            else if (activeIndex < min){
+                activeIndex = max;
+            }
+
+            return activeIndex;
+        }
+
+        function scrollThumbs(direction){
+            var isNeedle = (props.stageIndex != 0 && (props.stageIndex % (props.visibleThumbs-1) == 0));
+
+            if (props.stageIndex == 0 && props.oldStageIndex == $objects.slides.length-1){
+                $objects.thumbsScrollable.animate({ marginLeft: 0 }, settings.scrollSpeed);
+            }
+            else if (props.oldStageIndex == 0 && props.stageIndex == $objects.slides.length-1){
+                $objects.thumbsScrollable.animate({
+                    marginLeft: -(props.initialOffset * ($objects.slides.length/props.visibleThumbs))
+                }, settings.scrollSpeed);
+            }
+            else if (isNeedle){
+                var sign = null;
+
+                switch(direction){
+                    case 'prev':
+                        sign = '+=';
+                        break;
+
+                    case 'next':
+                        sign = '-=';
+                        break;
+
+                    default:
+                        sign = (props.stageIndex > props.oldStageIndex ? '-=' : '+=');
+                        break;
+                }
+
+                $objects.thumbsScrollable.animate({ marginLeft: sign+props.initialOffset }, settings.scrollSpeed);
+            }
+        }
+    }
+})(jQuery);
